2300d0d2375a37a8da4bae874c9ed571e22359ad
nmdzw2910 <nmdzw2910@gmail.com>
2023-11-11 16:57:44 +0700

    Minor fix UI

--- CodePulse.UI/src/app/core/models/domain/product-image.ts
+++ CodePulse.UI/src/app/core/models/domain/product-image.ts
@@ -1,6 +1,4 @@
 export interface ProductImage {
   id: string;
-  title?: string;
-  description?: string;
   url: string;
 }
--- CodePulse.UI/src/app/globals.ts
+++ CodePulse.UI/src/app/globals.ts
@@ -1,4 +1,4 @@
 export class Globals {
   public static s3BaseUrl: string =
-    'https://nmd-code-pulse.s3.ap-southeast-1.amazonaws.com';
+    'https://nmd-code-pulse.s3.ap-southeast-1.amazonaws.com/';
 }
